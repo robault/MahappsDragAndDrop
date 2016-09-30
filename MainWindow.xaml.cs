d2071b77d39a06ab83b2cce286e35af1c42615ce
Robert Ault <robault@live.com>
2016-09-30 15:31:59 -0700

    Showing listview works, canvas does not.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,8 +21,7 @@ namespace MahappsDragAndDrop
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        // From:
-        // https://social.msdn.microsoft.com/Forums/vstudio/en-US/942d19a8-b862-48aa-883a-d59ad6acaa2a/dragdrop-image-to-canvas?forum=wpf
+        // http://wpftutorial.net/DragAndDrop.html
 
         public MainWindow()
         {
@@ -34,20 +33,125 @@ namespace MahappsDragAndDrop
             SettingsFlyout.IsOpen = true;
         }
 
-        private void SettingsList_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        Point startPoint;
+
+        private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Image image = e.Source as Image;
-            DataObject data = new DataObject(typeof(ImageSource), image.Source);
-            DragDrop.DoDragDrop(image, data, DragDropEffects.Copy);
+            // Store the mouse position
+            startPoint = e.GetPosition(null);
         }
 
-        private void DesignSurface_Drop(object sender, DragEventArgs e)
+        private void List_MouseMove(object sender, MouseEventArgs e)
         {
-            ImageSource image = e.Data.GetData(typeof(ImageSource)) as ImageSource;
-            Image imageControl = new Image() { Width = image.Width, Height = image.Height, Source = image };
-            Canvas.SetLeft(imageControl, e.GetPosition(DesignSurface).X);
-            Canvas.SetTop(imageControl, e.GetPosition(DesignSurface).Y);
-            DesignSurface.Children.Add(imageControl);
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+            {
+                // Get the dragged ListViewItem
+                ListView listView = sender as ListView;
+                ListViewItem listViewItem =
+                    FindAnchestor<ListViewItem>((DependencyObject)e.OriginalSource);
+
+                if (listViewItem != null)
+                {
+                    // Find the data behind the ListViewItem
+                    Image contact = (Image)listView.ItemContainerGenerator.
+                        ItemFromContainer(listViewItem);
+
+                    // Initialize the drag & drop operation
+                    DataObject dragData = new DataObject("myFormat", contact);
+                    DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+                }
+            }
+        }
+
+        // Helper to search up the VisualTree
+        private static T FindAnchestor<T>(DependencyObject current)
+            where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            while (current != null);
+            return null;
+        }
+
+        private void DropList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") ||
+                sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void DropList_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                Image contact = e.Data.GetData("myFormat") as Image;
+                ListView source = contact.Parent as ListView;
+                source.Items.Remove(contact);
+                source.Items.Refresh();
+                
+                // drop into the listview
+                Image listImage = new Image();
+                listImage.Source = contact.Source;
+                listImage.Height = contact.Height;
+                listImage.Width = contact.Width;
+                DropList.Items.Add(listImage);
+
+                // drop into the canvas
+                Image canvasImage = new Image();
+                canvasImage.Source = contact.Source;
+                canvasImage.Height = contact.Height;
+                canvasImage.Width = contact.Width;
+                int left = 0 + (int)(canvasImage.Width * DropCanvas.Children.Count);
+                int top = 0 + (int)(canvasImage.Height * DropCanvas.Children.Count);
+                Canvas.SetLeft(canvasImage, left);
+                Canvas.SetTop(canvasImage, top);
+                DropCanvas.Children.Add(canvasImage);
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DragList.Items.Clear();
+            DropList.Items.Clear();
+            DropCanvas.Children.Clear();
+
+            Label note = new Label();
+            note.Content = "Left side is a ListView, right side is a Canvas.";
+            DropList.Items.Add(note);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Image contact = new Image();
+                contact.Width = 64;
+                contact.Height = 64;
+                contact.Margin = new Thickness(10);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(@"/MahappsDragAndDrop;component/mahapps.metro_med.png", UriKind.Relative);
+                image.EndInit();
+                contact.Source = image;
+                DragList.Items.Add(contact);
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsFlyout.IsOpen = false;
+            MetroWindow_Loaded(sender, e);
         }
     }
 }
